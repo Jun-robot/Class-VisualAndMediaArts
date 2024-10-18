d0ff337a649d73b2e681a3a7cbd61b7ff672711c
Jun-robot <saijun721.1@gmail.com>
2024-10-18 15:30:15 +0900

    全画面

--- oF_DoubleMonitor/src/main.cpp
+++ oF_DoubleMonitor/src/main.cpp
@@ -6,7 +6,7 @@
 int main( ){
     ofAppGLFWWindow setting;
     setting.setMultiDisplayFullscreen(true);
-    ofSetupOpenGL(&setting, 3840, 1080, OF_WINDOW);
+    ofSetupOpenGL(&setting, 3840, 1080, OF_FULLSCREEN);
     ofRunApp(new ofApp());
     
     //ofRunMainLoop();
--- oF_DoubleMonitor/src/ofApp.cpp
+++ oF_DoubleMonitor/src/ofApp.cpp
@@ -2,17 +2,41 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+//    for(int n=0; n<NUM; n++){
+//        p[n].setup();
+//    }
+    Particle tmp;
+    tmp.setup();
+    p.push_back(tmp);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    for(int n=0; n < p.size(); n++){
+        p[n].update();
+        //境界条件 バウンダリーコンディション
+        if(p[n].pos.x>ofGetWidth()||p[n].pos.x<0)p[n].speed.x*=-1.0;
+        if(p[n].pos.y>ofGetHeight()||p[n].pos.y<0)p[n].speed.y*=-1.0;
+    }
 
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    //ofSetColor(255, 255, 255);
+    
+    for(int n=0; n < p.size(); n++){
+        ofSetColor(p[n].color);
+        p[n].draw();
+        
+        for(int k=n; k<p.size(); k++){
+            float length = sqrt((p[n].pos.x - p[k].pos.x)*(p[n].pos.x - p[k].pos.x)
+            + abs(p[n].pos.y - p[k].pos.y)*abs(p[n].pos.y - p[k].pos.y));
+            if(length<400)ofDrawLine(p[n].pos.x, p[n].pos.y, p[k].pos.x, p[k].pos.y);
+        }
+    }
+    
+    
 }
 
 //--------------------------------------------------------------
@@ -42,7 +66,9 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    Particle tmp;
+    tmp.setup();
+    p.push_back(tmp);
 }
 
 //--------------------------------------------------------------
@@ -76,6 +102,6 @@ void ofApp::gotMessage(ofMessage msg){
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo){
 
 }
