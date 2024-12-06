d1a3ebb60d34bb27e27ecf2069e4d68336812df7
Jun-robot <saijun721.1@gmail.com>
2024-12-06 16:20:04 +0900

    表示を簡単にした

--- of_ImageSequence/src/ofApp.cpp
+++ of_ImageSequence/src/ofApp.cpp
@@ -23,21 +23,11 @@ void ofApp::setup(){
     //exp.setAutoExit(true);
     exp.startExport();
 #endif
-    
-    Particle tmp;
-    tmp.setup();
-    p.push_back(tmp);
+
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-    for(int n=0; n < p.size(); n++){
-        p[n].update();
-        //境界条件 バウンダリーコンディション
-        if(p[n].pos.x>ofGetWidth()||p[n].pos.x<0)p[n].speed.x*=-1.0;
-        if(p[n].pos.y>ofGetHeight()||p[n].pos.y<0)p[n].speed.y*=-1.0;
-    }
-    
     
 }
 
@@ -48,17 +38,8 @@ void ofApp::draw(){
                     //FBOを使う時は、最初に真っ黒に塗り直さないといけない。
     
     ofSetColor(255, 255, 255);
-    for(int n=0; n < p.size(); n++){
-        ofSetColor(p[n].color);
-        p[n].draw();
-        
-        for(int k=n; k<p.size(); k++){
-            float length = sqrt((p[n].pos.x - p[k].pos.x)*(p[n].pos.x - p[k].pos.x)
-            + abs(p[n].pos.y - p[k].pos.y)*abs(p[n].pos.y - p[k].pos.y));
-            if(length<400)ofDrawLine(p[n].pos.x, p[n].pos.y, p[k].pos.x, p[k].pos.y);
-        }
-    }
-//    ofDrawCircle(ofGetWidth()/2, ofGetHeight()/2, radius);
+    ofDrawCircle(ofGetWidth()/2, ofGetHeight()/2, 100*sin(++cnt/60.0));
+    
     exp.end(); //保存
     exp.draw(0,0); //表示
 }
